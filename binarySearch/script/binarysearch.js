8ee3edce02050d07b6dfb4e0d6892e60d6cfd839
4ubaka447 <y.n.semenchenko@mail.ru>
2019-06-02 17:55:55 +0300

    Исправляет код до рабочего состояния, + console.log для отладки

--- binarySearch/script/binarysearch.js
+++ binarySearch/script/binarysearch.js
@@ -4,47 +4,43 @@
 // list - рабочий массив
 
 function binarySearch(value, list) {
-
+    console.log(list);
     // позиция искомого элемента
-    let itemPosition = -1;
+    let position = -1;
 
-    // позиция первого (нулевого) элемента массива
+    // позиция первого (нулевого) элемента отрезка
     let firstElementPosition = 0;
     // позиция последнего элемента массива
     let lastElementPosition = list.length - 1;
-    // позиция среднего элемента массива
+    // позиция среднего элемента отрезка
     let middleElementPosition;
 
     // флаг цикла
     let found = false;
 
-    // найдем середину массива
-    // с учетом того, что массив может быть нечетной длины, используем округление до ближайшего меньшего
-    middleElementPosition =
-        Math.floor((firstElementPosition + lastElementPosition) / 2);
-
     while (found === false && firstElementPosition <= lastElementPosition) {
+        // найдем середину массива
+        // с учетом того, что массив может быть нечетной длины, используем округление до ближайшего меньшего
+        middleElementPosition = Math.floor((firstElementPosition + lastElementPosition) / 2);
+        console.log(middleElementPosition); //для отладки
         // искомый элемент в середине массива
         if (middleElementPosition == value) {
-            itemPosition = middleElementPosition;
+            position = middleElementPosition;
             found = true;
         } else {
             // искомый элемент в левой части массива
             if (list[middleElementPosition] > value) {
                 lastElementPosition = middleElementPosition - 1;
+                console.log('last ' + lastElementPosition);
             }
             // искомый элемент в правой части массива
             else {
                 firstElementPosition = middleElementPosition + 1;
+                console.log('first ' + firstElementPosition);
             }
         }
+        console.log('-----')
     }
 
-    return itemPosition;
+    return position;
 }
-
-let a = 5;
-let b = [0, 1, 2, 3 , 4, 5, 6, 7, 8];
-
-console.log(binarySearch(a, b));
-
--- binarySearch/script/test.js
+++ binarySearch/script/test.js
@@ -1,7 +1,7 @@
 describe("binarySearch", function () {
 
     it("возвращает позицию искомого элемента", function () {
-        assert.equal(binarySearch(4, [0, 1, 2, 3 , 4, 5, 6, 7, 8]), 4);
+        assert.equal(binarySearch(5, [0, 1, 2, 3 , 4, 5, 6, 7, 8]), 5);
     });
 
 });
