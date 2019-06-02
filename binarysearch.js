9da75f47b38158cd8e2693ade2405cb5fdefc20b
4ubaka447 <y.n.semenchenko@mail.ru>
2019-06-02 16:20:41 +0300

    "Добавляет реализацию бинарного поиска"

--- binarysearch.js
+++ binarysearch.js
@@ -1,3 +1,44 @@
+// функция бинарного поиска элемента в массиве
+// возвращает позицию элемента
+// value - искомый элемент
+// list - рабочий массив
+
+// позиция искомого элемента
+let itemPosition;
+
+// позиция первого (нулевого) элемента массива
+let firstElementPosition = 0;
+// позиция последнего элемента массива
+let lastElementPosition = list.length - 1;
+// позиция среднего элемента массива
+let middleElementPosition;
+
+// флаг цикла
+let found = false;
+
+
 function binarySearch(value, list) {
+
+    // найдем середину массива
+    // с учетом того, что массив может быть нечетной длины, используем округление до ближайшего меньшего
+    middleElementPosition =
+        Math.floor((firstElementPosition + lastElementPosition) / 2);
+
+    while (found === false && firstElementPosition <= lastElementPosition) {
+        // искомый элемент в середине массива
+        if (middleElementPosition == value) {
+            itemPosition = middleElementPosition;
+        } else {
+            // искомый элемент в левой части массива
+            if (list[middleElementPosition] > value) {
+                lastElementPosition = middleElementPosition - 1;
+            }
+            // искомый элемент в правой части массива
+            else {
+                firstElementPosition = middleElementPosition + 1;
+            }
+        }
+    }
+
     return itemPosition;
 }
