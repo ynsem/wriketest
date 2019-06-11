0ea2d5283ec3e87103edf8aa633026946de77363
4ubaka447 <y.n.semenchenko@mail.ru>
2019-06-11 22:14:27 +0300

    Добавляет проверку массива на сортированность вверх или вниз

--- binarySearch/script/binarysearch.js
+++ binarySearch/script/binarysearch.js
@@ -1,73 +1,90 @@
+'use strict';
+
+// функция проверки массива на сортировку по возрастанию и убыванию
+// вернет true если массив сортирован или состоит из одинаковых элементов
+// list - рабочий массив
+function checksArray(list) {
+    let arraySortedAscending;
+    let arraySortedDescending;
+
+    // проверим массив на сортированность по возрастанию
+    for (let i = 0; i < list.length - 1; i++) {
+        if (list[i] > list[i + 1]) {
+            arraySortedAscending = false;
+        }
+    }
+
+    // проверим массив на сортированность по убыванию
+    for (let i = 0; i < list.length - 1; i++) {
+        if (list[i] < list[i + 1]) {
+            arraySortedAscending = false;
+        }
+    }
+
+    // если массив не возрастающий и не убывающий
+    if (arraySortedAscending === false && arraySortedDescending === false) {
+        return false;
+    }
+    return true;
+}
+
 // функция бинарного поиска элемента в массиве
 // возвращает позицию элемента
 // value - искомый элемент
 // list - рабочий массив
-
-// идея вынести эту штуку модулем, но почему-то не работает
-// import { checksArray } from './checksarray';
-
-// временно (пока не починим модульность) добавим функцию проверки массива сюда
-function checksArray(list) {
-    for (let i = 0; i < list.length - 1; i++) {
-        if (list[i] >= list[i + 1]) { // >= используем чтобы отсечь массивы с повторяющимеся элементами
-            return false;
-        }
-    }
-
-    return true;
-}
-
 function binarySearch(value, list) {
-
 
     let checksarray = checksArray(list);
 
-    console.log(list);
     // позиция искомого элемента
     let position = -1;
 
     // позиция первого (нулевого) элемента отрезка
     let firstElementPosition = 0;
+
     // позиция последнего элемента отрезка
     let lastElementPosition = list.length - 1;
+
     // позиция среднего элемента отрезка
     let middleElementPosition;
 
     // флаг цикла
     let found = false;
 
-    // проверка того, что рабочий массив сортирован по возрастанию
-    // выйдет из цикла сразу как найдет неупорядоченные соседние элементы
-    // дальнейший код закоментирован, и оставлен просто для понимания логики, а реализация вынесена в тдельную функцию,
-    // т.о. при улучшении алгоритма проверки, переписывать ничего не придется
-
+    // проверка на сортированность
     if (checksarray) {
+
         while (found === false && firstElementPosition <= lastElementPosition) {
             // найдем середину массива
             // с учетом того, что массив может быть нечетной длины, используем округление до ближайшего меньшего
             // + защитимся от переполнения при очень большой длине массива
-            middleElementPosition = Math.floor((firstElementPosition + (lastElementPosition - firstElementPosition) / 2));  
-            console.log(middleElementPosition); //для отладки
+            middleElementPosition = Math.floor((firstElementPosition + (lastElementPosition - firstElementPosition) / 2));
             // искомый элемент в середине массива
             if (middleElementPosition == value) {
                 position = middleElementPosition;
                 found = true;
             } else {
-                // искомый элемент в левой части массива
-                if (list[middleElementPosition] > value) {
-                    lastElementPosition = middleElementPosition - 1;
-                    console.log('last ' + lastElementPosition);
-                }
-                // искомый элемент в правой части массива
-                else {
-                    firstElementPosition = middleElementPosition + 1;
-                    console.log('first ' + firstElementPosition);
+                // 1я ветка - работает с массивом сортированным по возрастанию, или из одинаковых элементов
+                if (list[firstElementPosition] <= list[lastElementPosition]) {
+                    // искомый элемент в левой части массива
+                    if (value < list[middleElementPosition]) {
+                        lastElementPosition = middleElementPosition - 1;
+                    } else { // искомый элемент в правой части массива
+                        firstElementPosition = middleElementPosition + 1;
+                    }
+                } else {          // 2я ветка - работает с массивом сортированным по убыванию
+                    if (value > list[middleElementPosition]) {
+                        lastElementPosition = middleElementPosition - 1;
+                    }
+                    // искомый элемент в правой части массива
+                    else {
+                        firstElementPosition = middleElementPosition + 1;
+                    }
                 }
             }
-            console.log('-----')
         }
+    }
 
-        return position;
-
-    } else return "Не подходит для бинарного поиска";
+    return position;
 }
+
--- binarySearch/script/checksarray.js
+++ binarySearch/script/checksarray.js
@@ -1,9 +1,26 @@
-export function checksArray(list) {
-    for (let i = 0; i < list.length - 1; i++) {
-        if (list[i] >= list[i + 1]) {
-            return false;
-        }
-    }
+// функция проверки массива на сортировку по возрастанию и убыванию
+// list - рабочий массив
+// function checksArray(list) {
+//     let arraySortedAscending;
+//     let arraySortedDescending;
 
-    return true;
-}
+//     // проверим массив на сортированность по возрастанию
+//     for (let i = 0; i < list.length - 1; i++) {
+//         if (list[i] > list[i + 1]) {
+//             arraySortedAscending = false;
+//         }
+//     }
+
+//     // проверим массив на сортированность по убыванию
+//     for (let i = 0; i < list.length - 1; i++) {
+//         if (list[i] < list[i + 1]) {
+//             arraySortedAscending = false;
+//         }
+//     }
+
+//     // если массив не возрастающий и не убывающий
+//     if (arraySortedAscending === false && arraySortedDescending === false) {
+//         return false;
+//     }
+//     return true;
+// }
